b530d7caca7df5c64fc9cc8c6cc6094669d26ea8
Koen <koen.schilders@student.fontys.nl>
2018-03-14 12:04:02 +0100

    Added another comment.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,14 +3,20 @@ public class Calculator {
     /**
      * Adds two integers.
      *
-     * @param a Integer 1
-     * @param b Integer 2
-     * @return the addition of the two integers.
+     * @param a Integer 1.
+     * @param b Integer 2.
+     * @return the result of the addition of the two integers.
      */
     public int add(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Subtracts two integers.
+     * @param a Integer 1.
+     * @param b Integer 2.
+     * @return the result of the subtraction of the two integers.
+     */
     public int subtract(int a, int b) {
         return a - b;
     }
