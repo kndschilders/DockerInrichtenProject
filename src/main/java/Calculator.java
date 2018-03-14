af938c1a84fc0ccdf9a058ad535877e53f1e5b94
Koen <koen.schilders@student.fontys.nl>
2018-03-14 11:41:02 +0100

    Added comment.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,12 @@
 public class Calculator {
 
+    /**
+     * Adds two integers.
+     *
+     * @param a Integer 1
+     * @param b Integer 2
+     * @return the addition of the two integers.
+     */
     public int add(int a, int b) {
         return a + b;
     }
