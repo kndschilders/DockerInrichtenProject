d56feda058d0b92b5be3d41ec69ed1cb15021dac
Thomas van Dongen <thomasvandongen019@hotmail.com>
2018-04-11 15:58:43 +0200

    added performance test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -40,22 +40,22 @@ public class CalculatorTest {
     public void tearDown() {
     }
 
-    @Test
+    @Test(timeout=1)
     public void testAdd() {
         Assert.assertEquals(2, calc.add(1, 1));
     }
 
-    @Test
+    @Test(timeout=1)
     public void testSubtract() {
         Assert.assertEquals(0, calc.subtract(1, 1));
     }
 
-    @Test
+    @Test(timeout=1)
     public void testMultiply() {
         Assert.assertEquals(1, calc.multiply(1, 1));
     }
 
-    @Test
+    @Test(timeout=1)
     public void testDivide() {
         Assert.assertEquals(1, calc.divide(1, 1));
     }
